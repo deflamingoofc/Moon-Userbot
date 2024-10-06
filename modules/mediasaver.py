326c1f7e8749c228307a26ceaa2c79e67e0bc498
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-06 22:45:13 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -18,7 +18,8 @@ async def msave(client: Client, message: Message):
     path = await message.reply_to_message.download()
     # await getattr(client, "send_" + media)("me", path)
     await client.send_document("me", path)
-
+    os.close(path)
+    
 
 modules_help["mediasaver"] = {
     "ms": "Save self-destructing media and send it to Saved Messages",
