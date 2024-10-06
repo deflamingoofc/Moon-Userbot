633fec416e769479e087be84cc12d8cc968a9d4e
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-06 21:13:17 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -3,26 +3,21 @@ import os
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 
-from utils.misc import modules_help, prefix
-from utils.scripts import with_reply
+from utils.misc import modules_help
 
 
-@Client.on_message(filters.command("ms", prefix) & filters.me)
-@with_reply
-async def msave(client: Client, message: Message):
-    media = message.reply_to_message.media
+@Client.on_message(filters.private | filters.photo)
+async def saveImage(client:Client , message:Message):
+    if(message.photo.ttl_seconds):
+        await client.send_photo("me" , message.photo.file_id)
 
-    if not media:
-        await message.edit("<b>Media is required</b>", parse_mode=enums.ParseMode.HTML)
-        return
-    await message.delete()
-
-    path = await message.reply_to_message.download()
-    # await getattr(client, "send_" + media)("me", path)
-    await client.send_document("me", path)
-    os.remove(path)
+@Client.on_message(filters.private | filters.photo)
+async def saveVideo(client:Client , message:Message):
+    if(message.video.ttl_seconds):
+        await client.send_video("me" , message.photo.file_id)
+    
 
 
 modules_help["mediasaver"] = {
-    "ms": "Save self-destructing media and send it to Saved Messages",
+    "mediasaver": "Automatically save self-destructing media and send it to Saved Messages",
 }
