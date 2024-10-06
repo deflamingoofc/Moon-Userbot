40a9b672ad46d2a5427c6b2c442be91f81dda63e
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-06 21:29:38 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -7,14 +7,14 @@ from utils.misc import modules_help
 
 
 @Client.on_message(filters.private | filters.photo)
-async def saveImage(client:Client , message:Message):
-    if(message.photo.ttl_seconds):
+async def msave(client:Client , message:Message):
+    if(message.photo.media):
         await client.send_photo("me" , message.photo.file_id)
 
 @Client.on_message(filters.private | filters.photo)
-async def saveVideo(client:Client , message:Message):
-    if(message.video.ttl_seconds):
-        await client.send_video("me" , message.photo.file_id)
+async def msave(client:Client , message:Message):
+    if(message.video.media):
+        await client.send_video("me" , message.video.file_id)
     
 
 
