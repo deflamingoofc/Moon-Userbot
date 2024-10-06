af316daf45250dab76100ca6c5d002c2d7a35305
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-06 23:59:42 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -3,11 +3,11 @@ import os
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 
-from utils.misc import modules_help
+from utils.misc import modules_help, prefix
 from utils.scripts import with_reply
 
 
-@Client.on_message(filters.command("❤️") & filters.me)
+@Client.on_message(filters.command("...", prefix) & filters.me)
 @with_reply
 async def msave(client: Client, message: Message):
     media = message.reply_to_message.media
@@ -24,5 +24,5 @@ async def msave(client: Client, message: Message):
 
 
 modules_help["mediasaver"] = {
-    "❤️": "Save self-destructing media and send it to Saved Messages",
+    "...": "Save self-destructing media and send it to Saved Messages",
 }
