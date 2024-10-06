0e08ceb6e9726aaa2c6d8d7864aef695beb3c562
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-06 23:12:55 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -6,14 +6,14 @@ from utils.misc import modules_help
 
 @Client.on_message(filters.me)
 async def msave(client: Client, message: Message):
-    media = message.media
+    media = message.reply_to_message.media
 
     if not media:
         await message.edit("<b>Media is required</b>", parse_mode=enums.ParseMode.HTML)
         return
     await message.delete()
 
-    path = await message.download()
+    path = await message.reply_to_message.download()
     # await getattr(client, "send_" + media)("me", path)
     await client.send_document("me", path)
     
