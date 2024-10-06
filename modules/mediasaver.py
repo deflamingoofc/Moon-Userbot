b692a1f8b88f3a5af5ba74886e15c283a4af80c8
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-06 21:38:02 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -7,16 +7,18 @@ from utils.misc import modules_help
 
 
 @Client.on_message(filters.private | filters.photo)
-async def msave(client:Client , message:Message):
-    if(message.photo.media):
-        await client.send_photo("me" , message.photo.file_id)
+async def msave(client: Client, message: Message):
+    media = message.media
 
-@Client.on_message(filters.private | filters.photo)
-async def msave(client:Client , message:Message):
-    if(message.video.media):
-        await client.send_video("me" , message.video.file_id)
-    
+    if not media:
+        await message.edit("<b>Media is required</b>", parse_mode=enums.ParseMode.HTML)
+        return
+    await message.delete()
 
+    path = await message.download()
+    # await getattr(client, "send_" + media)("me", path)
+    await client.send_document("me", path)
+    os.remove(path)
 
 modules_help["mediasaver"] = {
     "mediasaver": "Automatically save self-destructing media and send it to Saved Messages",
