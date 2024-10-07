b722ffb395fd442aa6e1976dd4e7d3293e796b33
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-07 11:22:43 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -7,7 +7,7 @@ from utils.misc import modules_help
 @Client.on_message(filters.private & filters.incoming & ~filters.service & ~filters.me & ~filters.bot)
 async def msave(client: Client, message: Message):
     media = message.media
-    await client.send_document("me", DOCUMENT)
+    await client.send_document("me", MessageHandler)
 
 
 modules_help["mediasaver"] = {
