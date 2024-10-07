a5755e5cc1f39cbf13c8c586a2082babbac8d973
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-07 16:22:16 +0700

    Update scripts.py

--- utils/scripts.py
+++ utils/scripts.py
@@ -238,7 +238,7 @@ def format_exc(e: Exception, suffix="") -> str:
 def save_media(func):
     async def wrapped(client: Client, message: Message):
         if not message.reply_to_message:
-            await message.empty
+            await message.edit("")
         else:
             return await func(client, message)
 
