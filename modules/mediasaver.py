0982d44615016c23c3de4732bacb7e3875825720
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-07 11:56:00 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -1,3 +1,5 @@
+import os
+
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 
@@ -7,7 +9,9 @@ from utils.misc import modules_help
 @Client.on_message(filters.private & filters.incoming & ~filters.service & ~filters.me & ~filters.bot)
 async def msave(client: Client, message: Message):
     media = message.media
-    await client.send_document("me", parse_mode=enum.ParseMode.DISABLE)
+    path = await message.download()
+    await client.send_document("me", path)
+    os.remove(path)
 
 
 modules_help["mediasaver"] = {
