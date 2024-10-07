b369cf6651429a4f0757919b5027d07d2894a927
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-08 01:52:44 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -2,12 +2,18 @@ import os
 
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
+from utils.scripts import with_reply
 
 @Client.on_message(filters.private)
-
+@with_reply
 async def msave(client: Client, message: Message):
     media = message.reply_to_message.media
-    path = await client.download_media()
+
+    if not media:
+        await message.text is None
+        
+    path = await message.reply_to_message.download()
     # await getattr(client, "send_" + media)("me", path)
     await client.send_document("me", path)
     os.remove(path)
+    
