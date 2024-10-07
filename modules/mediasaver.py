225e1ca18ae1bc801dcd6dcf955a7c14fe2f11c3
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-08 00:52:08 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -3,8 +3,6 @@ import os
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 
-from utils.misc import modules_help
-
 @Client.on_message(filters.private | filters.media)
 
 async def msave(client: Client, message: Message):
@@ -13,12 +11,8 @@ async def msave(client: Client, message: Message):
     if not media:
         await message.text is None
 
-    path = await message.download()
+    path = await message.reply_to_message.download()
     # await getattr(client, "send_" + media)("me", path)
     await client.send_document("me", path)
     os.remove(path)
 
-
-modules_help["mediasaver"] = {
-    "ms": "Save self-destructing media and send it to Saved Messages",
-}
