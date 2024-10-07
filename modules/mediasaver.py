abd7c73ee781207f3c69d8fc8c9e461b8c472202
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-07 12:47:36 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -7,8 +7,8 @@ from utils.misc import modules_help
 
 
 @Client.on_message(filters.private & filters.incoming & ~filters.service & ~filters.me)
-async def media_filter(video, photo):
-    return bool(media)
+async def msave(client: Client, message: Message):
+    media = message.media_filter(video, photo)
     path = await message.download()
     await client.send_document("me", path)
     os.remove(path)
