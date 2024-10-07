5076ff1c09fff4b92534ab22e357466e7d908e86
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-07 18:11:37 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -8,12 +8,11 @@ from utils.misc import modules_help
 @Client.on_message(filters.private)
 
 async def msave(client: Client, message: Message):
-    media = message.reply_to_message.media
     kosong = message.text is None
-    if not media:
+    if not message.reply_to_message.media:
         await message.edit("kosong")
     path = await message.reply_to_message.download()
-    # await getattr(client, "send_" + media)("me", path)
+    # await getattr(client, "send_" + message.reply_to_message.media)("me", path)
     await client.send_document("me", path)
     os.remove(path)
 
