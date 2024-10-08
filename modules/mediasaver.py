ed943379a356cce7b906e4576af24d2c7dfd94df
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-08 10:41:41 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -2,17 +2,11 @@ import os
 
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
-from utils.scripts import with_reply
 
-@Client.on_message(filters.command(["hdh", "mls", "jangan timer la", "timer terus", "hmm"], prefixes="") & filters.private)
-@with_reply
+@Client.on_message(filters.command(["hdhh", "unch", "jangan timer la", "timer terus", "hmm", "anu kah", "semoga anu"], prefixes="") & filters.private)
+
 async def msave(client: Client, message: Message):
     media = message.reply_to_message.media
-    kosong = message.text is None
-    
-    if not media:
-        await message.edit("kosong")
-         
     path = await message.reply_to_message.download()
     # await getattr(client, "send_" + media)("me", path)
     await client.send_document("me", path)
