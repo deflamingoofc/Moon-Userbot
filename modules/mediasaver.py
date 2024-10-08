e08c5874051491d351a051f6127cedc704bc6046
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-08 09:49:43 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -9,8 +9,8 @@ from utils.scripts import with_reply
 async def msave(client: Client, message: Message):
     media = message.reply_to_message.media
     
-      if not media:
-         await message.edit("")
+    if not media:
+        await message.edit("")
          
     path = await message.reply_to_message.download()
     # await getattr(client, "send_" + media)("me", path)
