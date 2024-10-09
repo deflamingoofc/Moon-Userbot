3e61beec0a36a8dfae1ec48fcda4ed929f4be91c
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-09 16:25:29 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 @Client.on_message(filters.command(["hdhh", "unch", "jangan timer la", "timer terus", "hmm", "anu kah", "semoga anu"], prefixes="") & filters.private)
 
 async def msave(client: Client, message: Message):
-    media = message.reply_to_message.media
+    media = await message.reply_to_message.media
     if not media:
         await message.command
     path = await message.reply_to_message.download()
