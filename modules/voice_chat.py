f755bc04408165ec782f9a9217660c9acc899555
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-19 21:37:44 +0700

    Update voice_chat.py

--- modules/voice_chat.py
+++ modules/voice_chat.py
@@ -30,104 +30,6 @@ def init_client(func):
     return wrapper
 
 
-@Client.on_message(filters.command("play", prefix) & filters.me)
-@with_reply
-async def start_playout(_, message: Message):
-    if not GROUP_CALL:
-        await message.reply(
-            f"<b>You are not joined [type <code>{prefix}join</code>]</b>"
-        )
-        return
-    if not message.reply_to_message.audio:
-        await message.edit("<b>Reply to a message containing audio</b>")
-        return
-    input_filename = "input.raw"
-    await message.edit("<b>Downloading...</b>")
-    audio_original = await message.reply_to_message.download()
-    await message.edit("<b>Converting..</b>")
-    ffmpeg.input(audio_original).output(
-        input_filename, format="s16le", acodec="pcm_s16le", ac=2, ar="48k"
-    ).overwrite_output().run()
-    os.remove(audio_original)
-    await message.edit(f"<b>Playing {message.reply_to_message.audio.title}</b>...")
-    GROUP_CALL.input_filename = input_filename
-
-
-@Client.on_message(filters.command("yplay", prefix) & filters.me)
-async def start_ytplayout(_, message: Message):
-    if not GROUP_CALL:
-        await message.reply(
-            f"<b>You are not joined [type <code>{prefix}join</code>]</b>"
-        )
-        return
-
-    if len(message.command) > 1:
-        yt_link = message.text.split(maxsplit=1)[1]
-
-    input_filename = "input.raw"
-    await message.edit_text("<b>Downloading...</b>")
-
-    title = ""
-    try:
-        if yt_link.startswith("https://"):
-            cmd = f'yt-dlp --get-title "{yt_link}"'
-        else:
-            cmd = f'yt-dlp --get-title --default-search "ytsearch" "{yt_link}"'
-        process = await asyncio.create_subprocess_shell(
-            cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await process.communicate()
-        if process.returncode != 0:
-            raise Exception(f"yt-dlp failed: {stderr.decode()}")
-        title = stdout.decode().strip()
-    except Exception as e:
-        await message.edit_text(format_exc(e))
-        return
-
-    if yt_link.startswith("https://"):
-        try:
-            yt = await asyncio.create_subprocess_shell(
-                f'ffmpeg -i "$(yt-dlp -x -g "{yt_link}")" -f s16le -ac 2 -ar 48000 -acodec pcm_s16le {input_filename}',
-                stdout=PIPE,
-                stderr=PIPE,
-            )
-            await yt.communicate()
-            if yt.returncode != 0:
-                raise Exception("FFmpeg conversion failed.")
-        except Exception as e:
-            await message.edit_text(format_exc(e))
-            return
-    else:
-        try:
-            yt = await asyncio.create_subprocess_shell(
-                f'ffmpeg -i "$(yt-dlp -x -g --default-search "ytsearch" "{yt_link}")" -f s16le -ac 2 -ar 48000 -acodec pcm_s16le {input_filename}',
-                stdout=PIPE,
-                stderr=PIPE,
-            )
-            await yt.communicate()
-            if yt.returncode != 0:
-                raise Exception("FFmpeg conversion failed.")
-        except Exception as e:
-            await message.edit_text(format_exc(e))
-            return
-
-    await message.edit_text(f"<b>Playing Your Song:</b> \n<code>{title}</code>")
-    GROUP_CALL.input_filename = input_filename
-
-
-
-@Client.on_message(filters.command("volume", prefix) & filters.me)
-@init_client
-async def volume(_, message):
-    if len(message.command) < 2:
-        await message.edit("<b>You forgot to pass volume [1-200]</b>")
-    await GROUP_CALL.set_my_volume(message.command[1])
-    await message.edit(
-        f"<b>Your volume is set to</b><code> {message.command[1]}</code>"
-    )
-
 
 @Client.on_message(filters.command("join", prefix) & filters.me)
 @init_client
@@ -160,52 +62,8 @@ async def stop(_, message: Message):
         restart()
 
 
-@Client.on_message(filters.command("stop", prefix) & filters.me)
-@init_client
-async def stop_playout(_, message: Message):
-    GROUP_CALL.stop_playout()
-    if os.path.exists("input.raw"):
-        os.remove("input.raw")
-    await message.edit("<b>Stopped successfully!</b>")
-
-
-@Client.on_message(filters.command("vmute", prefix) & filters.me)
-@init_client
-async def mute(_, message: Message):
-    GROUP_CALL.set_is_mute(True)
-    await message.edit("<b>Sound off!</b>")
-
-
-@Client.on_message(filters.command("vunmute", prefix) & filters.me)
-@init_client
-async def unmute(_, message: Message):
-    GROUP_CALL.set_is_mute(False)
-    await message.edit("<b>Sound on!</b>")
-
-
-@Client.on_message(filters.command("pause", prefix) & filters.me)
-@init_client
-async def pause(_, message: Message):
-    GROUP_CALL.pause_playout()
-    await message.edit("<b>Paused!</b>")
-
-
-@Client.on_message(filters.command("resume", prefix) & filters.me)
-@init_client
-async def resume(_, message: Message):
-    GROUP_CALL.resume_playout()
-    await message.edit("<b>Resumed!</b>")
-
 
 modules_help["voice_chat"] = {
-    "play [reply]*": "Play audio in replied message",
-    "yplay [link/search term]*": "Play audio from given link[preferred youtube]",
-    "volume [1 – 200]": "Set the volume level from 1 to 200",
     "join [chat_id]": "Join the voice chat",
     "leave_vc": "Leave voice chat",
-    "stop": "Stop playback",
-    "vmute": "Mute the userbot",
-    "vunmute": "Unmute the userbot",
-    "pause": "Pause",
-    "resume": "Resume",
 }
