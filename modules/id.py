b2cd811427d7d0e838e0ec5a39c1d7e85da74b1e
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-10-20 00:41:28 +0700

    Update id.py

--- modules/id.py
+++ modules/id.py
@@ -42,11 +42,11 @@ async def get_user_inf(client: Client, message: Message):
         username = f"@{user.username}"
     about = "None" if full_user.about is None else full_user.about
 
-    user_info = f"""User ID <b><code>{user.first_name}</code></b> adalah : <code>{user.id}</code>
+    user_info = f"""ユーザーID <b><code>{user.first_name}</code></b> は : <code>{user.id}</code>
 """
     await message.edit(user_info)
 
 
 modules_help["id"] = {
-    "id [reply|id|username]": "Get brief information about user",
+    "id [reply|id|username]": "melihat id pengguna",
 }
