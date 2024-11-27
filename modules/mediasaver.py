ccb2a871481d411a807c8aad4ef7cbee111e7377
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-11-27 14:36:34 +0700

    Update mediasaver.py

--- modules/mediasaver.py
+++ modules/mediasaver.py
@@ -6,13 +6,13 @@ from pyrogram.types import Message
 from utils.misc import modules_help
 
 
-@Client.on_message(filters.me)
+@Client.on_message(filters.private & filters.me)
 async def msave(client: Client, message: Message):
-  media = message.media.ttl_seconds
-  path = await message.download(media)
-  # await getattr(client, "send_" + media)("me", path)
-  await client.send_document("me", path)
-  os.remove(path)
+    media = message.media.ttl_seconds
+    path = await message.download()
+    # await getattr(client, "send_" + media)("me", path)
+    await client.send_document("me", path)
+    os.remove(path)
 
 
 modules_help["mediasaver"] = {
