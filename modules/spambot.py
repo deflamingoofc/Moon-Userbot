437e8b01b10340104efb2e11f3731b28b6e159fe
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-11-29 20:03:03 +0700

    Update spambot.py

--- modules/spambot.py
+++ modules/spambot.py
@@ -1,20 +1,13 @@
 import asyncio
-import os
 
 from pyrogram import Client, enums, filters, raw
 from pyrogram.types import Message
 
-from config import CMD_HANDLER as cmd
-from ProjectMan import *
-from ProjectMan.helpers.basic import edit_or_reply
-from ProjectMan.helpers.PyroHelpers import ReplyCheck
-from ProjectMan.helpers.tools import get_arg
-from ProjectMan.utils import s_paste
-
-from .help import *
+from utils.scripts import edit_or_reply
+from utils.misc import modules_help, prefix
 
 
-@Client.on_message(filters.command("limit", cmd) & filters.me)
+@Client.on_message(filters.command("limit", prefix) & filters.me)
 async def spamban(client: Client, m: Message):
     await client.unblock_user("SpamBot")
     response = await client.send(
@@ -30,3 +23,8 @@ async def spamban(client: Client, m: Message):
     spambot_msg = response.updates[1].message.id + 1
     status = await client.get_messages(chat_id="SpamBot", message_ids=spambot_msg)
     await wait_msg.edit_text(f"~ {status.text}")
+
+
+modules_help["spambot"] = {
+    "limit": "Check limit account Telegram",
+}
