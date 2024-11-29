a10d13fd10341dc2452aca1ee2e6bfe1f089577e
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-11-29 21:04:24 +0700

    Create tagalert.py

---
+++ modules/tagalert.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, enums, filters 
+from pyrogram.types import Message
+
+from utils.config import LOG_GROUP
+from utils.misc import modules_help, prefix
+log = []
+
+
+@Client.on_message(filters.command("tagalert on", ".") & filters.me)
+async def set_no_log_p_m(client: Client, message: Message):
+    if LOG_GROUP != -100:
+        if not message.chat.id in log:
+            log.append(message.chat.id)
+            await message.edit("**Tag alert Activated Successfully**")
+
+@Client.on_message(filters.command("tagalert off", ".") & filters.me)
+async def set_no_log_p_m(client: Client, message: Message):
+        if not message.chat.id in log:
+            log.remove(message.chat.id)
+            await message.edit("**Tag alert DeActivated Successfully**")
+
+if log:
+ @Client.on_message(filters.group & filters.mentioned & filters.incoming)
+ async def log_tagged_messages(client: Client, message: Message):
+    result = f"<b>📨 #TAGS #MESSAGE</b>\n<b> • : </b>{message.from_user.mention}"
+    result += f"\n<b> • Group : </b>{message.chat.title}"
+    result += f"\n<b> • 👀 </b><a href = '{message.link}'>Lihat Pesan</a>"
+    result += f"\n<b> • Message : </b><code>{message.text}</code>"
+    await asyncio.sleep(0.5)
+    await client.send_message(
+        LOG_GROUP,
+        result,
+        parse_mode=enums.ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
+
+
+modules_help["tagalert"] = {
+    "tagalert on/off": "To activate or deactivate the group tag, which will go to the log group.",
+}
