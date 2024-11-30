6596752c7297f67d27c65897ba65a919fb3910e1
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-11-30 08:12:08 +0700

    Update kickdel.py

--- modules/kickdel.py
+++ modules/kickdel.py
@@ -1,17 +1,26 @@
-from pyrogram import Client, filters
-
-from pyrogram.errors import (
-    UserAdminInvalid,
-    ChatAdminRequired,
-    RPCError,
-)
-
+from pyrogram import Client, ContinuePropagation, filters
 from pyrogram.types import Message, ChatPermissions
-
+from utils.db import db
 from utils.misc import modules_help, prefix
 from utils.handlers import (
     KickDeletedAccountsHandler,
 )
+
+db_cache: dict = db.get_collection("core.ats")
+
+
+def update_cache():
+    db_cache.clear()
+    db_cache.update(db.get_collection("core.ats"))
+    
+
+@Client.on_message(filters.group & ~filters.me)
+async def admintool_handler(_, message: Message):
+    if message.sender_chat and (
+        message.sender_chat.type == "supergroup"
+        or message.sender_chat.id == db_cache.get(f"linked{message.chat.id}", 0)
+    ):
+        raise ContinuePropagation
 
 
 @Client.on_message(filters.command(["kd"], prefix) & filters.me)
