92ddd52227700fc825d211e3906707198ecd3dbd
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-12-03 22:30:04 +0700

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -6,42 +6,30 @@
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 
-import os
-from time import perf_counter
-
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from pyrogram import Client, filters, enums
-from pyrogram.types import Message
-
-from utils.misc import modules_help, prefix
-from utils.scripts import with_rep
-
 #  GNU General Public License for more details.
 
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-@Client.on_message(filters.command(["jgn timer la", "hmm", "ehh", "timer trs"], prefixes="") & filters.me)
-@with_rep
-async def msave(client: Client, message: Message):
-    media = message.reply_to_message.media
-    path = await message.reply_to_message.download()
-    # await getattr(client, "send_" + media)("me", path)
-    await client.send_document("me", path)
-    os.remove(path)
+from time import perf_counter
 
-@Client.on_message(filters.command(["p"], prefix) & filters.me)
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.misc import modules_help, prefix
+
+
+@Client.on_message(filters.command(["ping", "p"], prefix) & filters.me)
 async def ping(_, message: Message):
     start = perf_counter()
     await message.edit("<b>Pong!</b>")
     end = perf_counter()
-    await message.edit(f"<b>Pong! {round(end - start, 3)}ms</b>")
+    await message.edit(f"<b>Pong! {round(end - start, 3)}s</b>")
 
 
 modules_help["ping"] = {
-    "p": "Check ping to Telegram servers",
+    "ping": "Check ping to Telegram servers",
 }
