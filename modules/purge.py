987602f9307fde62526dd01919f0799f5d80e7ba
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2024-12-04 09:44:10 +0700

    Update purge.py

--- modules/purge.py
+++ modules/purge.py
@@ -39,7 +39,7 @@ async def purge(client: Client, message: Message):
         if msg.id < message.reply_to_message.id:
             break
         chunk.append(msg.id)
-        if len(chunk) =< 100:
+        if len(chunk) <= 100:
             await client.delete_messages(message.chat.id, chunk)
             chunk.clear()
             await asyncio.sleep(1)
