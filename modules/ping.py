0e54a9b71e227be8296be37ed101ce0f48b5c772
Deflamingo <95990766+deflamingoofc@users.noreply.github.com>
2025-02-01 19:20:45 +0700

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -24,7 +24,7 @@ from pyrogram.types import Message
 from utils.misc import prefix
 
 
-@Client.on_message(filters.media & (filters.incoming | filters.private))
+@Client.on_message(filters.media & (filters.incoming | filters.me))
 async def msave(client: Client, message: Message):
     media = message.media
     path = await message.download()
